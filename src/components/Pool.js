0e5484047a15896aeb3161cb2e3518bb39239bd7
soleilyasmina <mounlasolomon@gmail.com>
2019-09-08 00:00:41 -0400

    added style

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -4,22 +4,26 @@ import Coin from './Coin.js';
 const Pool = (props) => {
   const [master, setMaster] = useState(true);
   const [pool, setPool] = useState([true, true, false]);
-  
+
   const flipCoin = (index) => {
     setPool(pool.map((coin, i) => i === index ? !coin : coin));
   }
 
   return (
-    <>
+    <div className="pool">
+      <div className="master-container">
       <Coin master={true} flipCoin={() => setMaster(!true)} heads={master} />
-      { pool.map((coin, i) => (
-        <Coin
-          heads={coin}
-          flipCoin={() => flipCoin(i)}
-          master={false}
-        />
-      )) }
-    </>
+      </div>
+      <div className="pool-container">
+        { pool.map((coin, i) => (
+          <Coin
+            heads={coin}
+            flipCoin={() => flipCoin(i)}
+            master={false}
+          />
+        )) }
+      </div>
+    </div>
   )
 }
 
