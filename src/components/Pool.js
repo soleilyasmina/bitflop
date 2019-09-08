462e33d09bb1ddafd52b86c5a2922bd528007816
soleilyasmina <mounlasolomon@gmail.com>
2019-09-08 00:16:58 -0400

    fixed background and added pool logic

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import Coin from './Coin.js';
 
 const Pool = (props) => {
-  const [master, setMaster] = useState(true);
+  const [master, setMaster] = useState(false);
   const [pool, setPool] = useState([true, true, false]);
 
   const flipCoin = (index) => {
     setPool(pool.map((coin, i) => i === index ? !coin : coin));
+    setMaster(!master);
   }
 
   return (
